219a66dca53273a29c0d3a05fbfe17315c99027b
Markus Thulin <macky_b@hotmail.com>
2021-06-05 21:17:41 +0200

    Add server.js

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const path = require('path');
+
+dotenv.config({ path: './config/config.env' });
+
+const app = express();
+
+//Middleware
+app.use(cors());
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.use('/', require('./routes/index'));
+
+const PORT = process.env.PORT || 4567;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));

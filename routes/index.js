7ebd2a1b93400a29163488b91a2df112db7ed7d4
Markus Thulin <macky_b@hotmail.com>
2021-06-10 22:14:25 +0200

    Added default route

---
+++ routes/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const router = express.Router();
+const dotenv = require('dotenv');
+const axios = require('axios');
+
+dotenv.config();
+
+router.get('/', function (req, response) {
+    const opt = {
+        headers: {
+            "X-Auth-Token": process.env.API_KEY,
+            'Accept': 'application/json'
+        }
+    };
+
+    axios.all([
+        axios.get(process.env.URL + "&group=Group%20A", opt),
+        axios.get(process.env.URL + "&group=Group%20B", opt),
+        axios.get(process.env.URL + "&group=Group%20C", opt),
+        axios.get(process.env.URL + "&group=Group%20D", opt),
+        axios.get(process.env.URL + "&group=Group%20E", opt),
+        axios.get(process.env.URL + "&group=Group%20F", opt)
+    ]).then(axios.spread((resp1, resp2, resp3, resp4, resp5, resp6) => {
+        response.set('Content-Type', 'text/html');
+        response.render('index',
+            {A: resp1.data, B: resp2.data,
+                C: resp3.data, D: resp4.data,
+                E: resp5.data, F: resp6.data});
+    })).catch(error => {
+        console.log(error);
+    });
+});
+
+module.exports = router;

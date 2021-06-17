3614fea4944f1028c71cdc96f12ccff71bb95cf0
Markus Thulin <macky_b@hotmail.com>
2021-06-17 12:53:14 +0200

    Added route for /scorers + default frontend

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,11 @@ const axios = require('axios');
 
 dotenv.config();
 
+/**
+ * Default Route, get group matches
+ *
+ *
+*/
 router.get('/', function (req, response) {
     const opt = {
         headers: {
@@ -14,12 +19,12 @@ router.get('/', function (req, response) {
     };
 
     axios.all([
-        axios.get(process.env.URL + "&group=Group%20A", opt),
-        axios.get(process.env.URL + "&group=Group%20B", opt),
-        axios.get(process.env.URL + "&group=Group%20C", opt),
-        axios.get(process.env.URL + "&group=Group%20D", opt),
-        axios.get(process.env.URL + "&group=Group%20E", opt),
-        axios.get(process.env.URL + "&group=Group%20F", opt)
+        axios.get(process.env.URL + "/matches?season=2021&group=Group%20A", opt),
+        axios.get(process.env.URL + "/matches?season=2021&group=Group%20B", opt),
+        axios.get(process.env.URL + "/matches?season=2021&group=Group%20C", opt),
+        axios.get(process.env.URL + "/matches?season=2021&group=Group%20D", opt),
+        axios.get(process.env.URL + "/matches?season=2021&group=Group%20E", opt),
+        axios.get(process.env.URL + "/matches?season=2021&group=Group%20F", opt)
     ]).then(axios.spread((resp1, resp2, resp3, resp4, resp5, resp6) => {
         response.set('Content-Type', 'text/html');
         response.render('index',
@@ -29,6 +34,29 @@ router.get('/', function (req, response) {
     })).catch(error => {
         console.log(error);
     });
+});
+
+/**
+ * /scorers Route, get top scorers
+ *
+ *
+*/
+router.get('/scorers', function (req, response) {
+    const opt = {
+        headers: {
+            "X-Auth-Token": process.env.API_KEY,
+            'Accept': 'application/json'
+        }
+    };
+
+    axios.get(process.env.URL + "/scorers", opt)
+        .then((resp) => {
+            response.set('Content-Type', 'text/html');
+            response.render('scorers', {result: resp.data});
+        })
+        .catch((error => {
+            console.log(error);
+        }));
 });
 
 module.exports = router;
